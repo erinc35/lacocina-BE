2e59e1fe9e84d8422f04996105e9d2bfb08aea5a
Erinc Emer <erincemer@gmail.com>
2019-06-11 12:53:19 -0700

    package-lock.json file deleted

--- api/recipes/recipeOwners/recipeOwnersRouter.js
+++ api/recipes/recipeOwners/recipeOwnersRouter.js
@@ -55,6 +55,7 @@ router.delete('/:id', async (req, res) => {
         res.status(200).json(deletedRecipe);
     } catch (err) {
         console.log(err)
+        
         res.status(500).json(err);
     }
 });
