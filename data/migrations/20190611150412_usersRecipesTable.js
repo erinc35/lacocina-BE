85758389d67a8114b3c7c8f421c3cb78f2f545c5
Erinc Emer <erincemer@gmail.com>
2019-11-09 17:14:32 -0800

    migrations reran

--- data/migrations/20190611150412_usersRecipesTable.js
+++ data/migrations/20190611150412_usersRecipesTable.js
@@ -3,7 +3,7 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('usersRecipes', function (tbl) {
         tbl.integer('userId').references('id').inTable('users').onDelete('CASCADE');
         tbl.integer('recipeId').references('id').inTable('recipes').onDelete('CASCADE');
-        tbl.integer('recipeImage').references('image').inTable('recipes').onDelete('CASCADE');        
+        tbl.string('recipeImage').references('image').inTable('recipes').onDelete('CASCADE');        
         tbl.timestamp('createdAt').defaultTo(knex.fn.now())
     })
 };
